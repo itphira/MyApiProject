6e4587e47fc959aa25ee708382e94a6a0082e0e5
itphira <it.soporte@phira.es>
2024-06-11 10:13:22 +0200

    update

--- obj/Debug/net8.0/MyApiProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyApiProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApiProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faa7bc9e40a9fa91c8941c0dd5b4aed72bf88fa7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+702a3ef18f5a7acfebc2bb1f5a6abdcdfc7184d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApiProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApiProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
